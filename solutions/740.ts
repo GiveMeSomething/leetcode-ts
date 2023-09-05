0fe18830726e93543a30f241f3123da9a5448096
Hoàng Tiến Minh <htminh.1001@gmail.com>
2023-09-06 01:34:13 +0700

    feat: add solution for problem 740 - Medium

---
+++ solutions/740.ts
@@ -0,0 +1,47 @@
+function deleteAndEarn(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+
+    let max = 0;
+    const maxPoint = Array(nums.length);
+    maxPoint[0] = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        let previous = 0;
+        for (let j = i - 1; j >= 0; j--) {
+            if (nums[j] === nums[i] - 1 || nums[j] === nums[i] + 1) {
+                continue;
+            }
+
+            if (nums[j] === nums[i]) {
+                previous += nums[i];
+                continue;
+            }
+
+            previous += maxPoint[j];
+            break;
+        }
+
+        if (previous > 0) {
+            max = Math.max(max, previous + nums[i]);
+        } else {
+            max = Math.max(max, nums[i]);
+        }
+
+        maxPoint[i] = max;
+        console.log(maxPoint);
+    }
+
+    return maxPoint[nums.length - 1];
+}
+
+export const main_740 = () => {
+    // const nums = [3, 4, 2];
+    // const nums = [2, 2, 3, 3, 3, 4];
+    const nums = [1, 1, 1, 2, 4, 5, 5, 5, 6];
+    // const nums = [8, 10, 4, 9, 1, 3, 5, 9, 4, 10];
+    // const nums = [8, 3, 4, 7, 6, 6, 9, 2, 5, 8, 2, 4, 9, 5, 9, 1, 5, 7, 1, 4];
+
+    const result = deleteAndEarn(nums);
+
+    console.log(result);
+};
