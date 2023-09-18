dcacea62942a78a0666f9ec8d7268373990fce89
Hoang Tien Minh <htminh.1001@gmail.com>
2023-09-18 16:15:00 +0700

    feat: add solution for problem 300 - Medium

--- solutions/300.ts
+++ solutions/300.ts
@@ -15,11 +15,24 @@
 //     return max;
 // }
 
-function lengthOfLIS(nums: number[]): number {}
+function lengthOfLIS(nums: number[]): number {
+    const sub: number[] = [nums[0]];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > sub[sub.length - 1]) {
+            sub.push(nums[i]);
+        } else {
+            sub[sub.findIndex((val) => val >= nums[i])] = nums[i];
+        }
+    }
+
+    return sub.length;
+}
 
 export const main_300 = () => {
     // const nums = [10, 9, 2, 5, 3, 7, 101, 18];
-    const nums = [1, 3, 6, 7, 9, 4, 10, 5, 6];
+    // const nums = [1, 3, 6, 7, 9, 4, 10, 5, 6];
+    const nums = [4, 10, 4, 3, 8, 9];
 
     const result = lengthOfLIS(nums);
     console.log(result);
